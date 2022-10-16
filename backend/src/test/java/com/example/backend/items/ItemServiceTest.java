a02f0198ed49a5cc850f8049791618b06d7c8d9d
Uthman Mohamed <83053931+1239uth@users.noreply.github.com>
2022-10-16 00:23:27 -0400

    Fix test failures due to CommandLineRunner

--- backend/src/test/java/com/example/backend/items/ItemServiceTest.java
+++ backend/src/test/java/com/example/backend/items/ItemServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.backend.items;
 
+import com.example.backend.category.Category;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -8,10 +9,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
+@SpringBootTest(properties = {"job.autorun.enabled=false"})
 class ItemServiceTest {
 
     @Autowired
@@ -23,12 +25,14 @@ class ItemServiceTest {
     @BeforeEach
     void setUp() {
         itemService = new ItemService(itemRepository);
+        Category category = new Category("clothing", new ArrayList<>());
+        Item item3 = new Item("shirt", "t-shirt", 19.99f, "https://www.pngitem.com/pimgs/m/172-1722226_t-shirt-purple-clip-art-blue-shirt-clipart.png", category);
 
-        Mockito.when(itemRepository.findAll()).thenReturn(new ArrayList<>());
+        Mockito.when(itemRepository.findByName("shirt")).thenReturn(Optional.of(item3));
     }
 
     @Test
     void getAllItems() {
-        assertEquals(0, itemService.getAllItems().size());
+        assertEquals("shirt", itemService.getByName("shirt").get().getName());
     }
 }
