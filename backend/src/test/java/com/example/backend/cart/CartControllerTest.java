afa3e0892d4eb759119f05f49eaa4fbdb9e3c48f
Uthman Mohamed <83053931+1239uth@users.noreply.github.com>
2022-10-18 21:32:22 -0400

    Add empty tests for repository and controller

---
+++ backend/src/test/java/com/example/backend/cart/CartControllerTest.java
@@ -0,0 +1,20 @@
+package com.example.backend.cart;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest
+public class CartControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    public void contextLoads() {
+
+    }
+}
---
+++ backend/src/test/java/com/example/backend/cart/CartRepositoryTest.java
@@ -0,0 +1,20 @@
+package com.example.backend.cart;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+public class CartRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    public void contextLoads() {
+
+    }
+}
