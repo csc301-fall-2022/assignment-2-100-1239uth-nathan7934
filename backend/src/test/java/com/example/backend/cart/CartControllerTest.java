c4a04664cebe08e26063fb564da564f79debf668
Uthman Mohamed <83053931+1239uth@users.noreply.github.com>
2022-10-18 20:53:49 -0400

    Add empty test classes for repository and controller

---
+++ backend/src/test/java/com/example/backend/cart/CartControllerTest.java
@@ -0,0 +1,12 @@
+package com.example.backend.cart;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest
+class CartControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
---
+++ backend/src/test/java/com/example/backend/cart/CartRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.example.backend.cart;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class CartRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
